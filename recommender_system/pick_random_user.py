fb2c7308880db56cfcacc0a3c90267336585b523
luctchak <luctchak@gmail.com>
2016-12-27 16:41:19 +0100

    fixing another bug

--- recommender_system/pick_random_user.py
+++ recommender_system/pick_random_user.py
@@ -5,7 +5,7 @@ import numpy as np
 def pick_random_user(data, num_users, minimum_number_of_films_rated):
     random_permut = np.random.permutation(range(0, num_users))
     for i in random_permut:
-        if len(data[:, 0][data[:, 0] == i] >= minimum_number_of_films_rated):
+        if len(data[:, 0][data[:, 0] == i]) >= minimum_number_of_films_rated:
             return i
     print 'No such element exist, reduce the number of film rated'
     return -1
